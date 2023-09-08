17588f060135f172134faece5c63aa65276a96d5
Aditya Arolkar <aditya.arolkar@berkeley.edu>
2023-09-08 04:05:10 -0700

    cosmwasm front end course: lesson 3 updates

--- scripts/upload_contract.ts
+++ scripts/upload_contract.ts
@@ -17,6 +17,25 @@ async function main(): Promise<void> {
    *  We're going to upload & initialise the contract here!
    *  Check out the video course on academy.cosmwasm.com!
    */
+
+  const mnemonic = getMnemonic();
+
+  const { client, address } = await connect(mnemonic, malagaConfig);
+
+  let {amount} = await client.getBalance(address, malagaConfig.feeToken);
+  if (amount == "0") {
+    console.warn('Not enough tokens on given wallet. Calling faucet!')
+    await hitFaucet(address, malagaConfig.feeToken, malagaConfig.faucetUrl);
+
+    let {amount} = await client.getBalance(address, malagaConfig.feeToken);
+    console.log('New balance of ${address} is ${amount}!');
+  }
+
+  const codeId = await uploadContracts(client, address, contracts);
+
+  const contractAddress = await initToken(client, address, codeId.cw20_base)
+
+  console.log(contractAddress);
 }
 
 main().then(
